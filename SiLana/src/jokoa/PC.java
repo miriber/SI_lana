53fc277b804d37a8c0c412c146e7ecc483a4ea0f
miriber <miriambergaz@iesbotikazarbhi.net>
2022-04-05 13:12:19 +0200

    05/04

--- SiLana/src/jokoa/PC.java
+++ SiLana/src/jokoa/PC.java
@@ -28,7 +28,7 @@ public class PC extends Jokalari {
 		while (PC.getNeureJok().badagoKokatuGabekoOntzirik()) {
 			x=random.nextInt(100);	//matrizeko buttonak 0-tik 100-ra arteko balioak izango balute moduan jokatu
 			y= x%10; //hondarra ateratzeko, honek y-ren balioa emango du
-			norabidea=chars
+			norabidea=chars[]
 			neureOntziakKokatu(x,y,norabidea);
 			aurkariarenFlota=Jokalari1.getNeureJok().getAurkariarenFlota();
 		}
